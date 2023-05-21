36427555d0ecd4060810d8d2c8e8f4af8b10d6d5
Dzihiko <anna.bond1702@gmail.com>
2023-05-21 17:32:06 +0300

    MaShA

---
+++ Example005_masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Text your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Huzzah, it is a Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
