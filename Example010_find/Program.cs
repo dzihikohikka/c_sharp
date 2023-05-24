890d5d3f525051bb1a51ec6e2f3053c42c646e6c
Dzihiko <anna.bond1702@gmail.com>
2023-05-25 02:54:13 +0300

    find num

---
+++ Example010_find/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 3, 45, 66, 76, 38, 26, 8, 2, 38 };
+
+int n = array.Length;
+int find = 38;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
