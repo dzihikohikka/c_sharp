cea6a31e0b8efd0122588de22d95d6c31c0f8f5e
Dzihiko <anna.bond1702@gmail.com>
2023-06-03 04:03:00 +0300

    сортировка до макс

---
+++ Example013maxSort/Program.cs
@@ -0,0 +1,33 @@
+﻿// сортировать массив от меньшего к большему
+
+int [] arr = { 5, 7, 5, 4, 9, 3, 45, 12, 1 };
+
+void PrintArray ( int[] arra)
+{
+    int count = arra.Length;
+  
+    for ( int i = 0; i < count; i++)
+    {
+        Console.Write($"{arra[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortMax(int [] ar)
+{
+    for (int i = 0; i < ar.Length-1; i++)
+    {
+        int minPos = i;
+            for ( int j = i + 1; j < ar.Length; j++)
+            {
+                if (ar[j] < ar [minPos]) minPos = j;
+            }
+        int temp = ar [i];
+        ar[i] = ar[minPos];
+        ar [minPos] = temp;
+    }
+}
+
+PrintArray(arr);
+SortMax(arr);
+PrintArray(arr);
