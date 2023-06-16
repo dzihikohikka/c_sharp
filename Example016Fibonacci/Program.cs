f812031fb8f807b5261d490476d4d1f9fdaf1499
Dzihiko <anna.bond1702@gmail.com>
2023-06-16 17:13:03 +0300

    рекурсия в числах фибоначи

---
+++ Example016Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1 1 2 5 8 13 (из каждых двух предыдущих)
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n ==2) return 1;
+    else return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+for (int i = 1; i<10; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
