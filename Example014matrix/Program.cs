39aaede2bdb9ba94c86b9730a4dfdcbe472b64d3
Dzihiko <anna.bond1702@gmail.com>
2023-06-16 17:11:25 +0300

    матрица пример

---
+++ Example014matrix/Program.cs
@@ -0,0 +1,29 @@
+﻿// пример вывода матрицы в консоль
+//                        i j    i строки j столбцы
+
+void PrintArray(int [,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int [,] matrix = new int [3,4];
+FillArray(matrix);
+PrintArray(matrix);
